0edcf1cef2e3fc85be4e7ed2bbda2b419560189c
fuatsevinc <fuatsevinc@icloude.com>
2022-06-16 22:00:54 +0200

    update

--- apiTodo/pagination.py
+++ apiTodo/pagination.py
@@ -1,6 +1,13 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
 class SmallPageNumberPagination(PageNumberPagination):
     page_size = 2
+    page_query_param = 'sayfa'
 
+class LargePageNumberPagination(PageNumberPagination):
+    page_size = 5 
+
+class MyLimitOfsetPagination(LimitOffsetPagination):
+    page_size = 2
+    limit_query_param = 'sayfadaki_eleman_sayisi' 
     
